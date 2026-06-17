8da9fb27ef9a01342db3199944894bb69e2bb43e
DEEPESH-GOEL <deepeshgoel.13@gmail.com>
2026-06-17 13:35:09 +0530

    add S3 bucket resource

--- main.tf
+++ main.tf
@@ -21,6 +21,21 @@ resource "aws_instance" "web" {
     ManagedBy   = "terraform"
   }
 }
+resource "aws_s3_bucket" "storage" {
+  bucket = "${var.instance_name}-${var.environment}-storage"
+
+  tags = {
+    Name        = "${var.instance_name}-${var.environment}-storage"
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  }
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.storage.bucket
+}
+
+
 
 output "public_ip" {
   value = aws_instance.web.public_ip
