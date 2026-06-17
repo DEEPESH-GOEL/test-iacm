630c89a2d703812455b370b3cea5fd3f5a2934bc
DEEPESH-GOEL <deepeshgoel.13@gmail.com>
2026-06-17 21:52:29 +0530

    restore s3 bucket

--- main.tf
+++ main.tf
@@ -1,20 +1,46 @@
 terraform {
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
 
-provider "aws" { region = var.region }
-
-module "ec2" {
-  source = "module.app.harness.io/l7HREAyVTnyfUsfUtPZUowN0dbHpRgRDOgg09wZK__gg/dg-ec2-module/aws"
-  version = "1.0.1"
-
-  ami           = var.ami
-  instance_type = var.instance_type
-  instance_name = var.instance_name
-  environment   = var.environment
+provider "aws" {
+  region = var.region
 }
 
-output "instance_id" { value = module.ec2.instance_id }
-output "public_ip"   { value = module.ec2.public_ip }
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  tags = {
+    Name        = "${var.instance_name}-${var.environment}"
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  }
+}
+resource "aws_s3_bucket" "storage" {
+  bucket = "${var.instance_name}-${var.environment}-storage"
+
+  tags = {
+    Name        = "${var.instance_name}-${var.environment}-storage"
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  }
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.storage.bucket
+}
+
+
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+output "instance_id" {
+  value = aws_instance.web.id
+}
